153b1e6e7492e2e74c3f8e0f452adfbc74b302cf
Shona Chan <mojavealoe@gmail.com>
2023-03-09 14:06:37 +0800

    adds displaying list index functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  get 'lists', to: 'lists#index'
 end
