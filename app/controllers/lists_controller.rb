a3f6d4844123ade32a755a4ebaa27ed1d665a122
Shona Chan <mojavealoe@gmail.com>
2023-03-09 15:16:11 +0800

    allows creation of new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,6 +13,15 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
 
@@ -20,9 +29,7 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
-  # def list_params
-  #   params.require(:list).permit(:name)
-  # end
-
-
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'lists/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-
-  get 'lists', to: 'lists#index'
-  get 'lists/new', to: 'lists#new'
-  get 'lists/:id', to: 'lists#show'
-
+  get 'lists', to: 'lists#index', as: :lists
+  get 'lists/new', to: 'lists#new', as: :new_list
+  get 'lists/:id', to: 'lists#show', as: :list
+  post 'lists', to: 'lists#create'
 end
