82cea1594ba79ef8d684169445debc32e1678bee
Shona Chan <mojavealoe@gmail.com>
2023-03-09 12:35:55 +0800

    adds validations for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true, uniqueness: true
+  validates :poster_url, presence: true
+  validates :rating, presence: true
 end
