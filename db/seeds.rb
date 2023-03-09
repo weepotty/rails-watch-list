d0ca0f9860a05a3582d1651d5d11fa87916c7670
Shona Chan <mojavealoe@gmail.com>
2023-03-10 02:08:56 +0800

    renders image associated with list on home page. updates new list form to allow user to input image path

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,6 @@ puts 'now seeding db...'
 url = 'https://tmdb.lewagon.com/movie/top_rated'
 array_of_movies = JSON.parse(URI.open(url).read)['results']
 
-
 array_of_movies.each do |movie|
   Movie.create(title: movie['original_title'], overview: movie['overview'], poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}", rating: movie['vote_average'] )
 end
