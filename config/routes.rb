97c15d71e4c9fa183da85a62eb88e5ae96fec71e
Shona Chan <mojavealoe@gmail.com>
2023-03-09 15:50:10 +0800

    displays form for new bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get 'lists', to: 'lists#index', as: :lists
-  get 'lists/new', to: 'lists#new', as: :new_list
-  get 'lists/:id', to: 'lists#show', as: :list
-  post 'lists', to: 'lists#create'
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: %i[new create]
+  end
 end
