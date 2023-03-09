62ff1183b98b88c162577d48ffff177a1cf62ab0
Shona Chan <mojavealoe@gmail.com>
2023-03-09 13:02:14 +0800

    creates bookmark model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_045152) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_050043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "comment"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name", null: false
@@ -31,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_045152) do
     t.index ["title"], name: "index_movies_on_title", unique: true
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
