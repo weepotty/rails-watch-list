9ed2b74e3739054c66de8d37541f99245e1ce69b
Shona Chan <mojavealoe@gmail.com>
2023-03-09 15:02:36 +0800

    adds show list details functionality

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,17 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
   def index
     @lists = List.all
+  end
+
+  def show
+    @list
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
 
 
   get 'lists', to: 'lists#index'
+  get 'lists/:id', to: 'lists#show'
 end
