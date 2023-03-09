e7313ff1c4b23cbe06c63af652910f0ca319d308
Shona Chan <mojavealoe@gmail.com>
2023-03-09 13:35:27 +0800

    removes presence validation on movie poster url and rating

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,4 @@
 class Movie < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
-  validates :poster_url, presence: true
-  validates :rating, presence: true, numericality: true
 end
