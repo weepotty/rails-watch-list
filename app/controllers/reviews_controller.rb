f15e9cf55d9660d996d8779b711f2bae6319ad0b
Shona Chan <mojavealoe@gmail.com>
2023-03-10 17:11:48 +0800

    refactors new review form into partial

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,12 @@
 class ReviewsController < ApplicationController
-
+  before_action :set_list, only: [:create]
 
   # POST lists/:list_id/reviews
   def create
-    @list = List.find(params[:list_id])
+    @list
     @review = Review.new(review_params)
     @review.list = @list
+    @bookmark = Bookmark.new
 
     if @review.save
       redirect_to list_path(@list)
@@ -20,4 +21,7 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:author, :stars, :content)
   end
 
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :list
 
   validates :author, presence: true
-  validates :stars, presence: true, numericality: { only_integer: true }
+  validates :stars, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..5) }
   validates :content, presence: true
 end
