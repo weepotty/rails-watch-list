cb32a732def44e8dd712feb83cdb56c514d3299a
Shona Chan <mojavealoe@gmail.com>
2023-03-10 15:09:56 +0800

    adds validations to review model and association to list model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :list
+
+  validates :author, presence: true
+  validates :stars, presence: true, numericality: { only_integer: true }
+  validates :content, presence: true
 end
